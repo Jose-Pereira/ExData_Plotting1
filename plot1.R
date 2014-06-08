7d64a06ce61a1f4f2eac715ee9cdf936ac8fe4c2
Jose Pereira <japn65@gmail.com>
2014-06-08 11:19:31 -0400

    Increasing the Efficiency of my script using fread

--- plot1.R
+++ plot1.R
@@ -1,19 +1,16 @@
 #The script assumes that the data is already on R working directory
 #and that the extracted file mantains it's original name
-power_data <-read.table("household_power_consumption.txt",sep = ";", header = TRUE, stringsAsFactors= FALSE, colClasses= c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings="?") 
-#converting dates into a date format
-dates <- as.Date(power_data[,1], "%d/%m/%Y")
-#subsetting to the dates i want
-#Search for dates 2007-02-02 y 2007-02-02
-Done <- grep ("2007-02-01",dates, value = FALSE)
-Dtwo <- grep ("2007-02-02",dates, value = FALSE)
-rows <- cbind(Done,Dtwo)
-install.packages ("reshape2")
-library ("reshape2")
-rows <- melt(rows)
-rows <- rows [,3]
-#subsetting the dates i need
-sub_data <- power_data [rows,]
+# im going to use the function "fread", in order to do so you must
+# have installed the library ("data.table") via install.packages ("data.table")
+
+dates <- c('1/2/2007','2/2/2007')
+#subsetting the data using the dates i need
+power_data <- fread("household_power_consumption.txt")[Date %in% dates]
+#converting from character to date format
+power_data$Date <- as.Date(power_data$Date, "%d/%m/%Y")
+#converting from character to numeric in order to do the histogram
+power_data$Global_active_power <-as.numeric(power_data$Global_active_power)
+#calling the device and doing the graph
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
-hist(sub_data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+hist(power_data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,32 +1,25 @@
 #The script assumes that the data is already on R working directory
-#and that the extracted file mantains it's original name
-power_data <-read.table("household_power_consumption.txt",sep = ";", header = TRUE, stringsAsFactors= FALSE, colClasses= c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings="?") 
-#converting dates into a date format
-dates <- as.Date(power_data[,1], "%d/%m/%Y")
-#subsetting to the dates i want
-#Search for dates 2007-02-02 y 2007-02-02
-Done <- grep ("2007-02-01",dates, value = FALSE)
-Dtwo <- grep ("2007-02-02",dates, value = FALSE)
-rows <- cbind(Done,Dtwo)
-install.packages ("reshape2")
-library ("reshape2")
-rows <- melt(rows)
-rows <- rows [,3]
-#subsetting the dates i need
-sub_data <- power_data [rows,]
-#Changing the timezone values to US
+#and that the extracted file mantains it's original name.
+# Im going to use the function "fread", in order to do so, you must
+# have installed the library ("data.table") via install.packages ("data.table")
+
+dates <- c('1/2/2007','2/2/2007')
+#subsetting the data using the dates i need
+power_data <- fread("household_power_consumption.txt")[Date %in% dates]
+#changing the timezone values in order to match the example
 Sys.setlocale("LC_TIME", "C")
-coltime <- strptime(paste(sub_data$Date, sub_data$Time), "%d/%m/%Y %H:%M:%S",tz = "GMT") 
-Nsub_data<- cbind (coltime,sub_data)
+#Creating a new colummn that takes into account not only the day but also the hour of the day 
+coltime <- strptime(paste(power_data$Date, power_data$Time), "%d/%m/%Y %H:%M:%S",tz = "GMT") 
+# Creating the graph
 png(filename = "plot4.png", width = 480, height = 480, units = "px")
 par (mfrow = c(2,2), mar = c(4,4,4,2), oma = c(1,1,0,0))
-with(Nsub_data,{
-  plot(Nsub_data$coltime,Nsub_data$Global_active_power, type="l", xlab= "",ylab= "Global Active Power")
-  plot(Nsub_data$coltime, Nsub_data$Voltage, type="l", xlab="datetime", ylab="Voltage")
-  plot(Nsub_data$coltime, Nsub_data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
-  lines(Nsub_data$coltime, Nsub_data$Sub_metering_2, type="l", col = "red")
-  lines(Nsub_data$coltime, Nsub_data$Sub_metering_3, type="l", col = "blue")
-  legend("topright",lty = 1, col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty = "n")
-  plot(Nsub_data$coltime, Nsub_data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+with(power_data,{
+  plot(coltime,power_data$Global_active_power, type="l", xlab= "",ylab= "Global Active Power")
+  plot(coltime, power_data$Voltage, type="l", xlab="datetime", ylab="Voltage")
+  plot(coltime, power_data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+    lines(coltime, power_data$Sub_metering_2, type="l", col = "red")
+    lines(coltime, power_data$Sub_metering_3, type="l", col = "blue")
+    legend("topright",lty = 1, col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty = "n")
+  plot(coltime, power_data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 })
 dev.off()
